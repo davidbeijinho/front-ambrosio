98083b2105a52cf6b4604043e5a15432c4f15d80
davidbeijinho <davidbeijinho@gmail.com>
2018-09-30 01:46:05 +0200

    fix espace string, add serve to test build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -330,7 +330,7 @@ module.exports = {
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
     new webpack.DefinePlugin({
-      __API_URL__: '//localhost:3000',
+      __API_URL__: '\'//localhost:3000\'',
     }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
