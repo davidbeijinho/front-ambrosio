486d5caa6c78225254e8b69376109d56ad300df3
davidbeijinho <davidbeijinho@gmail.com>
2018-10-16 00:44:55 +0200

    Add some basic styles and behavior

--- src/App.js
+++ src/App.js
@@ -1,74 +1,29 @@
-import React, { Component } from 'react';
+import React from "react";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Sensor from "./components/Sensor/Sensor";
+import Menu from "./components/Menu/Menu";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Grid } from 'semantic-ui-react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => (
+  <BrowserRouter>
+    <Grid>
+      <Grid.Row>
+        <Grid.Column>
+          <Menu />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
 
-    this.state = {
-      status: null,
-      sensor: null,
-    };
-  }
 
-  componentDidMount() {
-    fetch(process.env.REACT_APP_API_URL)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ status: data })
-        console.log('Status info', data)
-      });
-
-    fetch(process.env.REACT_APP_API_URL + '/api/sensor?filter[order]=id%20DESC&filter[limit]=3')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ sensor: data })
-        console.log('Sensor info', data)
-      });
-  }
-
-  getSensorInfo() {
-    if (this.state.sensor && this.state.sensor.length) {
-      return (
-        <ul>
-          {this.state.sensor.map((values) => <li key={values.id}>
-          <p>Temperature: {values.temperature}</p>
-            <ul>
-              <li>{values.id} | {values.created} </li>
-            </ul>
-          </li>)}
-        </ul>
-      )
-    } else if (this.state.sensor) {
-      return <p>No Sensor Information</p>
-    } else {
-      return <p>...Loading</p>
-    }
-  }
-
-  getStatusInfo() {
-    if (this.state.status) {
-      return (
-        <ul>
-          <li>Started: {this.state.status.started}</li>
-          <li>Uptime: {this.state.status.uptime}</li>
-        </ul>
-      )
-    } else {
-      return <p>...Loading</p>
-    }
-  }
-
-  render() {
-    console.log(this.state)
-    return (
-      <div>
-        <h1 className="App-title">Welcome to Ambrosio</h1>
-        {this.getStatusInfo()}
-        <hr />
-        {this.getSensorInfo()}
-      </div>
-    );
-  }
-}
-
+        <Grid.Column>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/sensor" component={Sensor} />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </BrowserRouter>
+);
 export default App;
